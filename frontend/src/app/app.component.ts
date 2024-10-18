28fcd59964f25db59f929f196171a7256b21b5f9
Anton udén <udenanton@hotmail.com>
2024-10-19 01:10:02 +0200

    Add socket.io

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SocketService } from './core/services/socket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(
+    private socket: SocketService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.socket.init();
+  }
 }
