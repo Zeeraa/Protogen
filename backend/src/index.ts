9acb86dc929c781f8793c5a54cbb2eca3e7e8a6a
Anton Uden <udenanton@hotmail.com>
2024-12-14 17:51:57 +0100

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,6 @@ import { loadConfiguration } from "./config/ConfigLoader";
 import { Protogen } from "./Protogen";
 import { red } from "colors";
 
-let protogen: Protogen | null = null;
 try {
   dotenv.config();
   consoleStamp(console);
@@ -14,17 +13,15 @@ try {
   const config = loadConfiguration();
 
   console.log("Creating server instance");
-  protogen = new Protogen(config);
+  const protogen = new Protogen(config);
 
   console.log("Protogen::init()");
   protogen.init().catch(err => {
     console.error(red("An error occured while calling Protogen::init()"), err);
     protogen?.visor.tryRenderTextFrame("BOOT ERROR :(\nInit failed!\nCheck logs", "#FF0000");
-    process.exit(1);
+    process.exit(2);
   });
 } catch (err) {
   console.error(red("An error occured while starting"), err);
-  console.log(protogen);
-  protogen?.visor.tryRenderTextFrame("BOOT ERROR :(\nFailed to start!\nCheck logs", "#FF0000");
   process.exit(1);
 }
