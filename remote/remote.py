85bab601fa1ef040eebe8c3bef0bb709a2e4354c
Anton Uden <udenanton@hotmail.com>
2025-01-29 17:08:03 +0100

    Got socket connection working

--- remote/remote.py
+++ remote/remote.py
@@ -1,9 +1,14 @@
+import sys
 import os
 import requests
+import asyncio
+import socketio
+import time
 
 from dotenv import load_dotenv
-from time import sleep
 from gpiozero import Button, MCP3008
+
+SOCKETIO_PATH = "/protogen-websocket.io"
 
 # ---------- Remote class ----------
 class Remote:
@@ -11,6 +16,7 @@ class Remote:
     self.api_url = api_url
     self.api_key = api_key
     self.use_direct_connection = use_direct_connection
+    self.websocket = None
     
     # Joystick ADC and button
     self.joystick_x = MCP3008(0)
@@ -22,8 +28,13 @@ class Remote:
     self.button_left = Button(4, pull_up=True)
     self.button_right = Button(6, pull_up=True)
   
+  #region Profile data loading
+  def check_profiles(self):
+    print("CHECK PROFILES")
+  #endregion
+  
   #region Start function
-  def start(self):
+  async def start(self):
     if self.use_direct_connection:
       print("Direct conection enabled. Trying to find optimal interface for communication")
       optimal_url = self.find_optimal_url()
@@ -32,6 +43,49 @@ class Remote:
       else:
         print("Found direct connection url " + optimal_url)
         self.api_url = optimal_url
+    
+    self.check_profiles()
+    
+    self.websocket = socketio.AsyncClient(reconnection=True, reconnection_attempts=0)
+    self.websocket.on("connect", self.on_connect)
+    self.websocket.on("connect_error", self.on_connect_error)
+    self.websocket.on("disconnect", self.on_disconnect)
+    
+    sensor_task = asyncio.create_task(self.read_sensors_and_send())
+    profile_update_task = asyncio.create_task(self.check_profiles_loop())
+    socket_connection_task = asyncio.create_task(self.connect_to_socket())
+    
+    await asyncio.gather(sensor_task, profile_update_task, socket_connection_task)
+  #endregion
+  
+  #region Websocket
+  def on_connect(self):
+    print("Connected to the websocket!")
+
+  def on_connect_error(self, data):
+    print("Socket connection failed:", data)
+
+  def on_disconnect(self):
+    print("Socket disconnected. Attempting to reconnect...")
+  
+  async def connect_to_socket(self):
+    backoff = 1  # Initial wait time in seconds
+    max_backoff = 30  # Maximum wait time
+    while True:
+      socket_url = ("wss://" + self.api_url[8:]) if self.api_url.startswith("https://") else ("ws://" + self.api_url[7:] if self.api_url.startswith("http://") else self.api_url)
+      try:
+        print(f"Attempting to connect to socket at {socket_url}...")
+        await self.websocket.connect(
+          socket_url,
+          transports=["websocket"],
+          socketio_path=SOCKETIO_PATH,
+          headers={"authorization": "Key " + self.api_key}
+        )
+        await self.websocket.wait()
+      except socketio.exceptions.ConnectionError:
+        print(f"Socket connection failed. Retrying in {backoff} seconds...")
+        await asyncio.sleep(backoff)
+        backoff = min(backoff * 2, max_backoff)
   #endregion
   
   #region Session id detection
@@ -50,7 +104,7 @@ class Remote:
   #region Direct connection discovery
   def find_optimal_url(self):
     headers = {
-      "X-api-key": self.api_key
+      "x-api-key": self.api_key
     }
     
     print("Trying to find optimal address to api")
@@ -82,6 +136,30 @@ class Remote:
     # No match :(
     return None
   #endregion
+  
+  #region Sensor readings for socket
+  async def read_sensors_and_send(self):
+    while True:
+      sensor_readings = {
+        "joystick_x": self.joystick_x.value,
+        "joystick_y": self.joystick_y.value,
+        "joystick_pressed": self.joystick_button.is_pressed,
+        "button_a": self.button_a.is_pressed,
+        "button_left": self.button_left.is_pressed,
+        "button_right": self.button_right.is_pressed,
+      }
+      if self.websocket.connected:
+        self.websocket.emit("message", {"type": "E2S_RemoteState", "data": sensor_readings})
+      await asyncio.sleep(0.25)
+  #endregion
+  
+  #region Get profiles
+  async def check_profiles_loop(self):
+    while True:
+      # Since first run is on start we wait before the next one
+      await asyncio.sleep(5)
+      self.check_profiles()
+  #endregion
     
 # ---------- Init code ----------
 if __name__ == "__main__":
@@ -93,4 +171,4 @@ if __name__ == "__main__":
 
   use_direct_connection = os.getenv("USE_DIRECT_CONNECTION") == "true"
   remote = Remote(url, api_key, use_direct_connection)
-  remote.start()
+  asyncio.run(remote.start())
