9ab082eb79f83e77b3a1813a61f23764d8c446ce
Anton Uden <udenanton@hotmail.com>
2025-02-01 00:07:56 +0100

    Initial code for handling remote actions

--- backend/src/remote/RemoteManager.ts
+++ backend/src/remote/RemoteManager.ts
@@ -1,3 +1,6 @@
+import { Equal } from "typeorm";
+import { RemoteControlActionType } from "../database/models/remote/RemoteControlActionType";
+import { SavedVideo } from "../database/models/video-player/SavedVideos.model";
 import { Protogen } from "../Protogen";
 import { KV_RemoteFlipAxis, KV_RemoteInvertX, KV_RemoteInvertY } from "../utils/KVDataStorageKeys";
 import { SocketMessageType } from "../webserver/socket/SocketMessageType";
@@ -41,6 +44,46 @@ export class RemoteManager {
     await this.protogen.database.setData(KV_RemoteInvertX, String(this.invertX));
     await this.protogen.database.setData(KV_RemoteInvertY, String(this.invertY));
     await this.protogen.database.setData(KV_RemoteFlipAxis, String(this.flipAxis));
+  }
+
+  public async performAction(type: RemoteControlActionType, action: string | null) {
+    //#region Start video playback
+    if (type == RemoteControlActionType.PLAY_VIDEO) {
+      const id = parseInt(action || "");
+      if (isNaN(id)) {
+        this.protogen.logger.error("Remote", "Invalid id passed");
+        return;
+      }
+
+      const repo = this.protogen.database.dataSource.getRepository(SavedVideo);
+
+      const video = await repo.findOne({
+        where: {
+          id: Equal(id),
+        }
+      });
+
+      if (video == null) {
+        this.protogen.logger.error("Remote", "Failed to play saved video since id was not found in database");
+        return;
+      }
+
+      if (video.isStream) {
+        await this.protogen.videoPlaybackManager.streamVideo(video.url);
+      } else {
+        await this.protogen.videoPlaybackManager.playVideo(video.url, video.mirrorVideo, video.flipVideo);
+      }
+
+      return true;
+    }
+    //#endregion
+
+    //#region Stop video playback
+    if (type == RemoteControlActionType.STOP_VIDEO) {
+      this.protogen.videoPlaybackManager.kill();
+    }
+    //#endregion
+    return false;
   }
 
   get protogen() {
