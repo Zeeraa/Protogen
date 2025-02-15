803e6813a3ccda36204c1a555266c061ca6469b4
Anton udén <udenanton@hotmail.com>
2025-02-15 17:47:44 +0100

    update dashboard page

--- frontend/src/app/features/dashboard/pages/dashboard-page/dashboard-page.component.ts
+++ frontend/src/app/features/dashboard/pages/dashboard-page/dashboard-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActionApiService, ActionSet } from '../../../../core/services/api/action-api.service';
+import { FaceApiService, FaceExpression } from '../../../../core/services/api/face-api.service';
+import { catchError } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -10,17 +13,33 @@ import { ActionApiService, ActionSet } from '../../../../core/services/api/actio
 })
 export class DashboardPageComponent implements OnInit {
   actions: ActionSet[] = [];
+  expressions: FaceExpression[] = [];
 
   constructor(
     private title: Title,
+    private toastr: ToastrService,
     private actionApi: ActionApiService,
+    private faceApi: FaceApiService,
   ) { }
 
   ngOnInit(): void {
     this.title.setTitle("Dashboard - Protogen");
 
-    this.actionApi.getActionSets().subscribe(actions => {
+    this.actionApi.getActionSets().pipe(
+      catchError(err => {
+        this.toastr.error("Failed to load actions");
+        throw err;
+      })
+    ).subscribe(actions => {
       this.actions = actions.filter(action => action.showOnDashboard);
     });
+
+    this.faceApi.getExpressions().pipe(
+      catchError(err => {
+        this.toastr.error("Failed to load expressions");
+        throw err
+      })).subscribe(expressions => {
+        this.expressions = expressions;
+      });
   }
 }
