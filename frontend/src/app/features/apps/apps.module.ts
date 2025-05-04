b45b6db57f32f52083e1880d0f06411fa6cd3108
Anton udén <udenanton@hotmail.com>
2025-05-04 19:28:36 +0200

    Initial app card page

--- frontend/src/app/features/apps/apps.module.ts
+++ frontend/src/app/features/apps/apps.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppsPageComponent } from './pages/apps-page/apps-page.component';
+import { AppCardComponent } from './components/app-card/app-card.component';
 
 @NgModule({
   declarations: [
-    AppsPageComponent
+    AppsPageComponent,
+    AppCardComponent
   ],
   imports: [
     CommonModule
--- frontend/src/app/features/apps/pages/apps-page/apps-page.component.ts
+++ frontend/src/app/features/apps/pages/apps-page/apps-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { App, AppsApi } from '../../../../core/services/api/apps-api.service';
+import { ToastrService } from 'ngx-toastr';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-apps-page',
@@ -6,6 +9,44 @@ import { Component } from '@angular/core';
   templateUrl: './apps-page.component.html',
   styleUrl: './apps-page.component.scss'
 })
-export class AppsPageComponent {
+export class AppsPageComponent implements OnInit, OnDestroy {
+  protected apps: App[] = [];
+  protected activeApp: App | null = null;
+  private interval: any = null;
 
+  constructor(
+    private appsApi: AppsApi,
+    private toastr: ToastrService,
+  ) { }
+
+  ngOnInit(): void {
+    this.loadApps();
+    this.interval = setInterval(() => {
+      this.getOpenApp();
+    }, 2500);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+
+  private loadApps() {
+    this.appsApi.getApps().pipe(catchError(err => {
+      this.toastr.error("Error loading apps");
+      throw err;
+    })).subscribe(appList => {
+      this.activeApp = appList.activeApp;
+      this.apps = appList.apps;
+    });
+  }
+
+  private getOpenApp() {
+    this.appsApi.getActiveApp().subscribe(activeApp => {
+      console.log(activeApp);
+    })
+  }
+
+  closeActiveApp() {
+    this.toastr.info("Close app function"); // TODO: api call
+  }
 }
