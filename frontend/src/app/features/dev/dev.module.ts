7ebd1a7976f460b7d8070f8a0182685cc642904d
Anton udén <udenanton@hotmail.com>
2025-06-08 14:33:52 +0200

    add visor preview in dev page

--- frontend/src/app/features/dev/dev.module.ts
+++ frontend/src/app/features/dev/dev.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DeveloperPageComponent } from './pages/developer-page/developer-page.component';
 import { DevLedPreviewComponent } from './components/dev-led-preview/dev-led-preview.component';
+import { VisorModule } from '../visor/visor.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { DevLedPreviewComponent } from './components/dev-led-preview/dev-led-pre
   ],
   imports: [
     CommonModule,
+    VisorModule,
   ],
   exports: [
     DeveloperPageComponent
