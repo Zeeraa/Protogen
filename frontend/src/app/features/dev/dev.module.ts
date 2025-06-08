4b6cdb93f410861d6a357ab2f945906e276f49ab
Anton udén <udenanton@hotmail.com>
2025-06-08 14:30:51 +0200

    show leds in dev tool

--- frontend/src/app/features/dev/dev.module.ts
+++ frontend/src/app/features/dev/dev.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DeveloperPageComponent } from './pages/developer-page/developer-page.component';
+import { DevLedPreviewComponent } from './components/dev-led-preview/dev-led-preview.component';
 
 @NgModule({
   declarations: [
     DeveloperPageComponent,
+    DevLedPreviewComponent,
   ],
   imports: [
     CommonModule,
